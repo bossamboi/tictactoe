a5c0dcaa54caca7f27ca6ee121cd5f8f2e56fc99
Eric Sohng <ericsohng@gmail.com>
2021-02-27 16:49:20 -0800

    added players ability to play X and O

--- script.js
+++ script.js
@@ -19,17 +19,46 @@ const gameBoard = (() => {
 
     for (let i = 0; i < board.length; i++) {
         let squareDiv = document.createElement('div');
+        squareDiv.textContent = board[i];
         squareDiv.classList.add('square-class');
+        squareDiv.classList.add('hover');
         squareContainer.appendChild(squareDiv);
     }
+
+    //does player creation go here?
+    const player1 = createPlayer('Player 1', 'X');
+    let player1Turn = true;
+    const player2 = createPlayer('Player 2', 'O');
+
+    let squares = document.querySelectorAll('.square-class');
+
+    squares.forEach(square => {
+        square.addEventListener('click', () => {
+            if (!square.textContent) {
+                if (player1Turn) {
+                    square.textContent = player1.marker;
+                    player1Turn = false;
+                } else {
+                    square.textContent = player2.marker;
+                    player1Turn = true;
+                }
+                square.classList.remove('hover');
+            }
+
+        })
+    } )
+
+    
 
 
 
 })();
 
 
-
-
-
-
 //game play object
+const gamePlay = (() => {
+    // const player1 = createPlayer('Eric', 'X');
+    // const player2 = createPlayer('Angela', 'O');
+
+
+})();
