4321f3005938da5e28bd8478f73fc5173560d87f
Eric Sohng <ericsohng@gmail.com>
2021-02-28 13:20:56 -0800

    added scripting that identifies whether there is a winner or tie

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 //player factory
 const createPlayer = (name, marker) => {
-    return {name, marker};
+    return { name, marker };
 }
 
 
 //game board object
 const gameBoard = (() => {
-    
+
     let board = [];
     for (let i = 0; i < 9; i++) {
         board.push('');
     }
 
     const winningCombos = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
     ]
 
     //populate board with squares 
@@ -31,6 +31,7 @@ const gameBoard = (() => {
         squareDiv.textContent = board[i];
         squareDiv.classList.add('square-class');
         squareDiv.classList.add('hover');
+        squareDiv.setAttribute('data-index', `${i}`);
         squareContainer.appendChild(squareDiv);
     }
 
@@ -51,15 +52,53 @@ const gameBoard = (() => {
                     square.textContent = player2.marker;
                     player1Turn = true;
                 }
+                board[+(square.dataset.index)] = square.textContent;
+                console.log(board);
                 square.classList.remove('hover');
+                check4Winner();
             }
 
-            
+
 
         })
-    } )
+    })
 
-    
+
+    const boardFilled = () => {
+        if (board.indexOf('') === -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const check4Winner = () => {
+        let xArray = getIndices(player1.marker);
+        let oArray = getIndices(player2.marker);
+
+        for (let i = 0; i < winningCombos.length; i++) {
+            if (winningCombos[i].every(index => { return xArray.indexOf(index) !== -1; })) {
+                console.log(player1.name + ' wins');
+                break;
+            } else if (winningCombos[i].every(index => { return oArray.indexOf(index) !== -1; })) {
+                console.log(player2.name + ' wins');
+                break;
+            } else if (boardFilled()) {
+                console.log('cat\'s game');
+                break;
+            }
+        }
+    }
+
+    const getIndices = (marker) => {
+        let xoArray = [];
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] === marker) {
+                xoArray.push(i);
+            }
+        }
+        return xoArray;
+    }
 
 
 
