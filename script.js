1fbdeb5fb00c12a41423d154fb040b45ff7d59e4
Eric Sohng <ericsohng@gmail.com>
2021-02-27 17:39:04 -0800

    eod commit

--- script.js
+++ script.js
@@ -12,7 +12,16 @@ const gameBoard = (() => {
         board.push('');
     }
 
-    console.log(board);
+    const winningCombos = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
 
     //populate board with squares 
     let squareContainer = document.querySelector('#square-container');
@@ -44,6 +53,8 @@ const gameBoard = (() => {
                 }
                 square.classList.remove('hover');
             }
+
+            
 
         })
     } )
